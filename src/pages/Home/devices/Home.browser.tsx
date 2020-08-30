464f346fca39de54f5388c4163441b2ffce3d761
Duy <huquduy@gmail.com>
2020-08-31 01:48:26 +0700

    update

--- src/pages/Home/devices/Home.browser.tsx
+++ src/pages/Home/devices/Home.browser.tsx
@@ -1,12 +1,10 @@
-import { ExpansionPanel, ExpansionPanelDetails, ExpansionPanelSummary, Grid, Paper, Tab, Tabs, Typography } from '@material-ui/core'
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
+import { Container, ExpansionPanel, ExpansionPanelDetails, ExpansionPanelSummary, Grid, Typography } from '@material-ui/core'
+import { ExpandMore, NotificationsNone } from '@material-ui/icons'
 import Bottom from 'components/Bottom'
 import Header from 'components/Header'
 import Link from 'components/Link'
-import TabPanel from 'components/TabPanel'
 import { imageSrc } from 'config'
-import GAMES, { getGameType, IProviderProps, SLOT_TAB } from 'constant/games'
-import { AuthContext } from 'contexts/authContext'
+import GAMES from 'constant/games'
 import useLoading from 'hooks/loading'
 import { map } from 'ramda'
 import React, { useEffect, useMemo, useState } from 'react'
@@ -27,16 +25,9 @@ const settingsCarousel = {
 };
 
 const Home: React.FC = () => {
-  const { auth } = React.useContext(AuthContext)
-  const [activeTab, setActiveTab] = useState(SLOT_TAB)
   const [runningText, setRunningText] = useState('')
-  const isLogged = !auth.token.length
   const [, withLoading] = useLoading(false)
-  const [expanded, setExpanded] = React.useState<string | false>(false)
-
-  const handleChangeTab = (event: React.ChangeEvent<{}>, newValue: string) => {
-    setActiveTab(newValue);
-  };
+  const [expanded, setExpanded] = React.useState<string | false>('1')
 
   const handleChangeExpand = (panel: string) => (event: React.ChangeEvent<{}>, newExpanded: boolean) => {
     setExpanded(newExpanded ? panel : false)
@@ -54,10 +45,8 @@ const Home: React.FC = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
-  const { providers }: { providers: IProviderProps[] } = useMemo(() => getGameType(activeTab), [activeTab])
-
   return (
-    <div className='home-page'>
+    <div className='browser-home-page'>
       <Header />
 
       {/* Carousel */}
@@ -65,80 +54,40 @@ const Home: React.FC = () => {
         {
           map((item: string) =>
             <div key={item}>
-              <img className='logo' alt='hokibet188' src={`${imageSrc}/home-carousel/${item}`} />
+              <img className='slide' alt='hokibet188' src={`${imageSrc}/home-carousel/${item}`} />
             </div>
           , carousels)
         }
       </Carousel>
-
-      {/* Authentication functions */}
-      {
-        isLogged ?
-          <div className='authentication'>
-            <Link className="register" href="/register">Register</Link>
-            <Link className="login" href="/login">Login</Link>
-          </div>
-          :
-          <div className='auth'>
-            <Link className="register" href="/deposit">Deposit</Link>
-            <Link className="transfer" href="/transfer">Transfer</Link>
-            <Link className="login" href="/withdraw">Withdraw</Link>
-          </div>
-      }
-      <MarqueeText className="marquee" text={runningText} duration={30} repeat={2} />
+      <Container className="announcement" maxWidth="lg">
+        <NotificationsNone />
+        <MarqueeText className="marquee" text={runningText} duration={30} repeat={2} />
+      </Container>
       {/* Provider list */}
-      <div className='game-tabs'>
-        <Tabs
-          className="custom_tab"
-          value={activeTab}
-          onChange={handleChangeTab}
-          indicatorColor="primary"
-          variant="scrollable"
-          scrollButtons="on"
-        >
-          {map(({ idName }: { idName: string }) => <Tab
-            key={idName}
-            label={
-              <div>
-                <img className='game-type-icon' alt='hokibet188' src={`${imageSrc}icons/${idName.toLocaleLowerCase()}.png`} />
-                <Typography variant="caption" display="block" gutterBottom={true}>
-                  {idName}
-                </Typography>
-              </div>
-            }
-            value={idName}
-          />, GAMES)}
-        </Tabs>
-        <TabPanel value={activeTab}>
-          <Grid container={true} spacing={1}>
-            {map(({ image, idName, route, target }: IProviderProps) =>
-              <Grid item={true} xs={4} sm={4} key={idName}>
-                <Link href={route} target={target}>
-                  <Paper className='provider'>
-                    <img className='logo' alt='hokibet188' src={`${imageSrc}providers/${image}`} />
-                    <Typography variant="caption" display="block" gutterBottom={true}>
-                      {idName}
-                    </Typography>
-                  </Paper>
-                </Link>
-              </Grid>
-            , providers)}
-          </Grid>
-        </TabPanel>
-      </div>
+      <Container maxWidth="lg">
+        <Grid container={true}>
+          {map(({ idName, route = '', title }) => {
+            return(
+              <Grid item={true} xs={3} className="home-icon">
+                <Link href={route}><img className='logo' alt='hokibet188' src={`${process.env.PUBLIC_URL}/images/home/${idName}.png`} /></Link>
+              </Grid>)
+          }, GAMES)}
+        </Grid>
+      </Container>
       {/* Text intro */}
       <div className="text-home">
         {map(({ id, title, content }) => 
-          <ExpansionPanel key={id} expanded={expanded === id} onChange={handleChangeExpand(id)}>
-            <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />} aria-controls={id} id={id} key={id}>
-              <h1>{title}</h1>
+          <ExpansionPanel color="primary" key={id} expanded={expanded === id} onChange={handleChangeExpand(id)}>
+            <ExpansionPanelSummary color="primary" expandIcon={<ExpandMore />} aria-controls={id} id={id} key={id}>
+              <Typography variant="h6" color="primary">{title}</Typography>
             </ExpansionPanelSummary>
-            <ExpansionPanelDetails key={id}>
-              <div dangerouslySetInnerHTML={{ __html: content }} />
+            <ExpansionPanelDetails color="primary" key={id}>
+              <Typography variant="body2" color="primary">
+                <div dangerouslySetInnerHTML={{ __html: content }} />
+              </Typography>
             </ExpansionPanelDetails> 
           </ExpansionPanel>, intro)}
       </div>
-      <Bottom />
     </div>
   )
 }
